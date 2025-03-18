9f18da988cc49b409a5f70c350f341949aa1eeaa
Gabriel <glima7591@gmail.com>
2025-03-18 11:26:23 -0300

    feat: Atualizar componente App e estilos
    - Modificado o App.tsx para exibir uma mensagem de saudação centralizada.
    - Adicionados diretivas do Tailwind CSS no index.css para estilização.
    - Atualizado o main.tsx para importar o novo arquivo index.css em vez do main.css.

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/explicit-function-return-type */
 function App() {
-  return <div></div>
+  return (
+    <div className="flex h-full items-center justify-center">
+      <span className="text-4xl text-blue-500">Hello from Electron</span>
+    </div>
+  )
 }
 
 export default App
